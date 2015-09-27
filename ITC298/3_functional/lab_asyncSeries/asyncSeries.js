bd8b3fa4b8c2fdc8738447548b6874669c42a1ca
StefanoGitHub <stefano.borghi@gmail.com>
2015-09-26 17:08:59 -0700

    comments and cleans asyncEach

--- ITC298/3_functional/lab_asyncSeries/asyncSeries.js
+++ ITC298/3_functional/lab_asyncSeries/asyncSeries.js
@@ -1,29 +1,40 @@
 //asyncSeries.js
 
-//var pause = require('./pause');
-
-
-var asyncSeries = function(tasks, done) {
-    return asyncSeriesRecursive(tasks, done, 0, []);
-};
-
+//loop through the tasks recursively
 var asyncSeriesRecursive = function (tasks, done, currentTaskIndex, results) {
-
+    //check if the current index in the tasks array exists
     if (currentTaskIndex < tasks.length) {
+        //call the correspondent task
         tasks[currentTaskIndex](function (err, currentResult) {
+            //once the callback is called check err
             if (err) {
+                //if error call done with err
                 done(err);
             }
 
+            //load the result of the task to the results array
             results.push(currentResult);
+            //call the next task, passing the results array
             asyncSeriesRecursive(tasks, done, currentTaskIndex + 1, results);
 
+            //if the current task is actually the last one, and done exists,
+            //call done (the callback of the asyncSeries function)
             if (currentTaskIndex == tasks.length - 1 && done) {
                 done(null, results);
             }
         });
     }
 };
+
+
+var asyncSeries = function(tasks, done) {
+    var startTaskIndex = 0;
+    var results = [];
+    //start a recursive loop from the first element (index = 0) and with an empty results array
+    asyncSeriesRecursive(tasks, done, startTaskIndex, results);
+};
+
+
 
 
 asyncSeries([
@@ -35,13 +46,16 @@ asyncSeries([
             console.log(2);
             callback();
         }, 1000);
-    }, console.log.bind(console, 3)
+    }, console.log.bind(console, 3) //here the callback is never called
+    //function(callback) {
+    //    console.log(3);
+    //    callback();
+    //}
 ], function(err) {
-    if (err) {
-        console.log("This should not run unless there are errors.");
-    }
+    console.log("This should not run unless there are errors."); //this message will always be logged
 });
 //logout -> 1  2 3
+
 
 
 asyncSeries([
