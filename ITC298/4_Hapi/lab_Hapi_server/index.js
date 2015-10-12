524e1e8de52af3843be276346257a0a3d31a26a4
StefanoGitHub <stefano.borghi@gmail.com>
2015-10-12 09:30:15 -0700

    new implementation of index.js
    memorizing the lines of the file in a variable, so the async reading is
    done only once on the first call

--- ITC298/4_Hapi/lab_Hapi_server/index.js
+++ ITC298/4_Hapi/lab_Hapi_server/index.js
@@ -5,6 +5,11 @@ var fs = require("fs");
 var server = new hapi.Server();
 server.connection({ port: 8000 });
 
+server.start(function(){
+    console.log('Server running!')
+    console.info(server.info);
+});
+
 server.views({
     engines: {
         html: require('handlebars')
@@ -12,38 +17,69 @@ server.views({
     path: "./"
 });
 
-var fileLines = [];
-//load the lines from the file
-fs.readFile("./fortune.txt", "utf-8", function(err, data) {
-    if (err) throw err;
-    fileLines = data.split("\n");
-    console.log('File loaded!')
-    //once the file has been loaded, start the server
-    server.start(function(){
-        console.log('Server running!')
-        console.info(server.info);
-    });
-});
 
-var showLink = function(request, reply) {
-    reply.view("template.html", {
-            link: 'get fortune',
-            line: '' 
-        });
+//var fileLines = [];
+////load the lines from the file
+//fs.readFile("./fortune.txt", "utf-8", function(err, data) {
+//    if (err) throw err;
+//    fileLines = data.split("\n");
+//    console.log('File loaded!')
+//    //once the file has been loaded, start the server
+//    server.start(function(){
+//        console.log('Server running!')
+//        console.info(server.info);
+//    });
+//});
+//
+//var returnLine = function(request, reply) {
+//    var x = request.params.x || 'random';
+//    var line = '';
+//    if (x == 'random' || x >= fileLines.length) {
+//        line = fileLines[Math.floor(Math.random() * fileLines.length)];
+//    } else {
+//        line = fileLines[x];
+//    }
+//    reply.view("template.html", {
+//        link: '',
+//        line: line
+//    }); 
+//};
+
+
+var getLinesMemo = function(callback) {
+    fs.readFile("./fortune.txt", "utf-8", function(err, data) {
+        if (err) return callback(err);
+        var lines = data.split("\n");
+        //replace the existing function with one that returns immediately
+        getLinesMemo = function(c) {
+            c(null, lines);
+        };
+        callback(null, lines);
+    });
 };
 
 var returnLine = function(request, reply) {
     var x = request.params.x || 'random';
     var line = '';
-    if (x == 'random' || x >= fileLines.length) {
-        line = fileLines[Math.floor(Math.random() * fileLines.length)];
-    } else {
-        line = fileLines[x];
-    }
+    getLinesMemo(function(err, fileLines) {
+        if (err) { throw err; }
+        if (x == 'random' || x >= fileLines.length) {
+            line = fileLines[Math.floor(Math.random() * fileLines.length)];
+        } else {
+            line = fileLines[x];
+        }
+        reply.view("template.html", {
+            link: '',
+            line: line
+        }); 
+    });
+};
+
+var showLink = function(request, reply) {
     reply.view("template.html", {
-        link: '',
-        line: line
-    }); 
+        link: 'get fortune',
+        line: '' 
+    });
 };
 
 server.route([
