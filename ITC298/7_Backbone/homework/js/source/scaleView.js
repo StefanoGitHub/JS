6231985831bf4c989333fb85e384cc0bd169c9ce
StefanoGitHub <stefano.borghi@gmail.com>
2015-10-24 21:25:24 -0700

    fixes Backbone homework

--- ITC298/7_Backbone/homework/js/source/scaleView.js
+++ ITC298/7_Backbone/homework/js/source/scaleView.js
@@ -8,24 +8,31 @@ var GalacticScaleView = Backbone.View.extend({
     template: _.template( $("#scale-template").html() ),
     events: {
         "click .planet": "calculateWeight",
+        "click #reset": "reset",
         "keyup #input": "updateWeight"
+    },
+    calculateWeight: function(e) {
+        //get data from the user
+        var selectedPlanet = $(e.target).attr("data-planet");
+        var input = $(e.view.$('#input')).val();
+        //call the function from the model
+        this.model.calculateWeight(selectedPlanet);
+        //render the page with the new data
+        this.render();
+        $(e.view.$('#input')).val(input);
+        $(e.view.$('[data-planet="'+selectedPlanet+'"]')).css("background-color", "cornflowerblue");
     },
     render: function() {
         var model = this.model.toJSON();
         var html = this.template(model);
         this.$el.html(html);
     },
-    calculateWeight: function(e) {
-        var planet = $(e.target).attr("data-planet");
-        var input = $(e.view.$('#input')).val();
-        this.model.calculateWeight(planet);
+    reset: function() {
+        this.model.set("result", '-');
+        this.model.set("input", 0);
+        this.model.set("image", 'galaxy.jpg');
+        this.model.set("description", 'Our galaxy');
         this.render();
-        $(e.view.$('#input')).val(input);
-
-        $(e.view.$(':button')).css("background-color", "none");// working??
-        //console.log($(e.view.$(':button'))); //works
-        $(e.target).css("background-color", "skyblue");//why not working?????
-        //$(e.view.$('body')).css('background-color', 'blue');//works
     },
     updateWeight: function(e) {
         var input = $(e.view.$('#input')).val();
